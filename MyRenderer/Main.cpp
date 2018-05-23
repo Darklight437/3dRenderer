5fcf5007e833ff44e3740495d2d602c02294e37a
Darklight437 <darklight437@gmail.com>
2018-05-23 10:10:24 +1000

    glm & glfw added
    includes set up too

--- MyRenderer/Main.cpp
+++ MyRenderer/Main.cpp
@@ -1,5 +1,29 @@
+#include<glfw\glfw3.h>
+
+
 int main()
 {
+	if (glfwInit() == false)
+	{
+		return -1;
+	}
+	//code!
 
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "hella sick graphics", nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+
+	glfwMakeContextCurrent(window);
+
+
+
+
+
+	glfwTerminate();
 	return 0;
 }
