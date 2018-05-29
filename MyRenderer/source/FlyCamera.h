e8a7768b464aa2c4137a4015f2532b7dbc2f7f22
Darklight437 <darklight437@gmail.com>
2018-05-29 16:35:33 +1000

    moved some classes and created a clock from other work

---
+++ MyRenderer/source/FlyCamera.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "c:\Users\S171097\Documents\Github\OpenGL render pipeline\3dRenderer\MyRenderer\source\Camera.h"
+class FlyCamera :
+	public Camera
+{
+public:
+	FlyCamera();
+	~FlyCamera();
+};
+
