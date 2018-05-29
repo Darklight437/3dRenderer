e672574815492521fd60a385ab058bf265f85836
Darklight437 <darklight437@gmail.com>
2018-05-29 16:38:36 +1000

    CLOCKSSSSS

--- MyRenderer/source/Clock.cpp
+++ MyRenderer/source/Clock.cpp
@@ -4,9 +4,50 @@
 
 Clock::Clock()
 {
+	m_createTime = m_timer.now();
+	m_lastTime = m_createTime;
 }
 
 
-Clock::~Clock()
+
+
+
+
+int64_t Clock::getTimeSeconds()
 {
+	return std::chrono::duration_cast<std::chrono::seconds>(m_timer.now() - m_createTime).count();
+}
+
+int64_t Clock::getTimeMilliseconds()
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds>(m_timer.now() - m_createTime).count();
+}
+
+int64_t Clock::getTimeMicroseconds()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(m_timer.now() - m_createTime).count();
+}
+
+void Clock::resetClock()
+{
+	m_createTime = m_timer.now();
+}
+
+void Clock::update()
+{
+	deltaTime = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(m_timer.now() - m_lastTime).count() / 1000.0f;
+	m_lastTime = m_timer.now();
+}
+
+float Clock::getDeltaTime()
+{
+	return deltaTime;
+}
+
+Clock& Clock::getInstance()
+{
+
+	static Clock clock;
+	return clock;
+
 }
--- MyRenderer/source/Clock.h
+++ MyRenderer/source/Clock.h
@@ -1,8 +1,41 @@
 #pragma once
+#include <chrono>
+
 class Clock
 {
 public:
+
+	int64_t getTimeSeconds();
+	int64_t getTimeMilliseconds();
+	int64_t getTimeMicroseconds();
+	void resetClock();
+
+
+	void update();
+
+	float getDeltaTime();
+	static Clock& getInstance();
+
+private:
+	//singleton tools
+	static bool instanceflag;
 	Clock();
-	~Clock();
+	Clock(Clock const&);
+	void operator=(Clock const&);
+
+
+	//functional elements
+	std::chrono::high_resolution_clock m_timer;
+
+	//the time that this clock was created at
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_createTime;
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_lastTime;
+
+
+
+	float deltaTime = 0.0f;
+
+};
 };
 
