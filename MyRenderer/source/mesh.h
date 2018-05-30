b6d9ed8e14efb78e150f5acb25bffe727b33cca6
Darklight437 <darklight437@gmail.com>
2018-05-30 14:29:30 +1000

    MESH BUT BIGGER
    added some struct elements

--- MyRenderer/source/mesh.h
+++ MyRenderer/source/mesh.h
@@ -1,13 +1,26 @@
 #pragma once
+#include <glm\vec4.hpp>
+#include <glm\vec2.hpp>
 class Mesh
 {
 public:
-	Mesh() : triCount(0), vao(0), vbo(0), ibo(0) {}
+	Mesh() : triCount(0), vao(0), vbo(0), ibo(0) {}
+
 	~Mesh();
+
+	struct vertex
+	{
+		glm::vec4 position;
+		glm::vec4 normal;
+		glm::vec2 texCoord
+
+	};
+
 
 protected:
 	unsigned int triCount;
-	unsigned int vao, vbo, ibo;
+	unsigned int vao, vbo, ibo;
+
 
 };
 
