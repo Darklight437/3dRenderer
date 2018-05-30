7908ea58b366a586981bfd09ea4c7c70f1bc9588
Darklight437 <darklight437@gmail.com>
2018-05-30 16:09:38 +1000

    mesh for quad done

--- MyRenderer/source/Mesh.cpp
+++ MyRenderer/source/Mesh.cpp
@@ -46,11 +46,32 @@ void Mesh::initialiseQuad()
 	//fill the vert buffer with these vertices
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(Vertex), vertices, GL_STATIC_DRAW);
 
+	glDisableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 
+	//unbind current buffers
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//quad has 2 tris so
+	triCount = 2;
 
 
 }
 
 void Mesh::draw()
 {
+	glBindVertexArray(vertArrObj);
+	//figure out if using vertices or indices
+	if (indexBuffObj != 0)
+	{
+		glDrawElements(GL_TRIANGLES, 3 * triCount, GL_UNSIGNED_INT, 0);
+	}
+	else
+	{
+		glDrawArrays(GL_TRIANGLES, 0, 3 * triCount);
+	}
+
+
+
 }
