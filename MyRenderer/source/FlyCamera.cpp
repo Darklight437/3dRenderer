1c85a78c05d6565300037dfda700767f19598245
Darklight437 <darklight437@gmail.com>
2018-06-12 16:09:45 +1000

    flying cameraaaaa

--- MyRenderer/source/FlyCamera.cpp
+++ MyRenderer/source/FlyCamera.cpp
@@ -1,9 +1,14 @@
 #include "FlyCamera.h"
-
-
+#include "Clock.h"
+#include <algorithm>
 
 FlyCamera::FlyCamera()
 {
+	updateViewMatrix();
+	updateProjectionMatrix();
+	updateProjectionViewMatrix();
+
+
 }
 
 
@@ -11,12 +16,53 @@ FlyCamera::~FlyCamera()
 {
 }
 
-void FlyCamera::update(float deltaTime)
+void FlyCamera::setPosition(const glm::vec3 position)
 {
+	m_position = position;
 
+	updateViewMatrix();
+	updateProjectionViewMatrix();
 }
 
-void FlyCamera::setspeed(float speed)
+void FlyCamera::processKeyboard(Camera_Movement direction)
 {
-	m_speed = speed;
+	float velocity = (m_running ? m_fastSpeed : m_moveSpeed) * Clock::getInstance().deltaTime();
+
+	switch (direction)
+	{
+	case FORWARD:
+		m_position += m_front * velocity;
+		break;
+	case BACKWARD:
+		m_position -= m_front * velocity;
+		break;
+	case LEFT:
+		m_position -= m_right * velocity;
+		break;
+	case RIGHT:
+		m_position += m_right * velocity;
+		break;
+	default:
+		break;
+	}
+
+	updateViewMatrix();
+	updateProjectionViewMatrix();
 }
+
+void FlyCamera::processMouseMovement(float xoffset, float yoffset)
+{
+	xoffset *= m_lookSensitivity;
+	yoffset *= m_lookSensitivity;
+
+	m_pitch += yoffset;
+	m_yaw += xoffset;
+
+	m_pitch = std::min(89.0f, m_pitch);
+}
+
+void FlyCamera::updateViewMatrix()
+{
+}
+
+
--- MyRenderer/source/FlyCamera.h
+++ MyRenderer/source/FlyCamera.h
@@ -1,19 +1,54 @@
 #pragma once
 #include "Camera.h"
-class FlyCamera :
-	public Camera
+#include "glfw\glfw3.h"
+
+
+enum Camera_Movement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+
+
+class FlyCamera :public Camera
 {
 public:
 	FlyCamera();
 	~FlyCamera();
 
-	void update(float deltaTime);
-	void setspeed(float speed);
+
+	bool m_running = false;
+
+
+	void setPosition(const glm::vec3 position);
+
+	void processKeyboard(Camera_Movement direction);
+	void processMouseMovement(float xoffset, float yoffset);
+
+
+	
 
 private:
 	float m_speed;
 	vec3 up;
 
+	void updateViewMatrix();
 
+	vec3 m_front = vec3(0, 0, 1);
+	vec3 m_right = vec3(1, 0, 0);
+	vec3 m_up = vec3(0, 1, 0);
+
+
+
+	float m_pitch = -45.0f;
+	float m_yaw = -45.0f;
+
+	float m_moveSpeed = 10.0f;
+	float m_fastSpeed = 50.0f;
+
+	float m_lookSensitivity = 0.1f;
 };
 
