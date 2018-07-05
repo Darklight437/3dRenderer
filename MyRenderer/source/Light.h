2be3aa2bcc81d9803f7a4ad794d7ddae4bf94165
Darklight437 <darklight437@gmail.com>
2018-07-05 22:21:30 +1000

    light stuff

--- MyRenderer/source/Light.h
+++ MyRenderer/source/Light.h
@@ -6,10 +6,19 @@ class Light
 public:
 	Light();
 	~Light();
+	void setStrength(float lightstrength) { m_lightStrength = lightstrength; }
+	float getStrength() { return m_lightStrength; }
+
+	void setColour(glm::vec4 lightColour) { m_lightColour = lightColour; }
+	glm::vec4 getColour() { return m_lightColour; }
+
+	void setTransform(glm::mat4 newTransform) { m_lightTransform = newTransform; }
+	glm::mat4 getTransform() { return m_lightTransform; }
+
 
 private:
-	glm::vec4 lightColour;
-	glm::mat4 loghtTransform;
-	float lightStrength;
+	glm::vec4 m_lightColour;
+	glm::mat4 m_lightTransform;
+	float m_lightStrength;
 };
 
